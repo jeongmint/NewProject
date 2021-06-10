bd0c77f16207c5c88f0d98801ae2016f211281c3
Jeong Min Choi <tangerine_t@naver.com>
2021-06-11 00:12:24 +0900

    Test...

--- firstGraphics.cpp
+++ firstGraphics.cpp
@@ -4,9 +4,8 @@
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
-  glShadeModel(GL_FLACT);
+  glShadeModel(GL_FLAT);
   glColor3f(0.0, 0.0, 1.0);
-  glLoadIdentify();
   gluLookAt(1.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // 변수가 9개나 들어간다..
   glScalef(1.0, 2.0, 1.0);
   glutWireTeapot(3.0);
@@ -17,7 +16,6 @@ void reshape(int w, int h)
 {
   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
   glMatrixMode(GL_PROJECTION);
-  glLoadIdentify();
   glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
   glMatrixMode(GL_MODELVIEW);
 }
@@ -29,7 +27,7 @@ int main(int argc, char** argv)
   glutInitWindowSize(800, 400);
   glutCreateWindow("OpenGL을 공부하기 위한 티팟 샘플!");
   glClearColor(1.0, 1.0, 1.0, 1.0);
-  glutDisplayFunc(display); // dispaly 메서드 호출
+  glutDisplayFunc(display); // display 메서드 호출
   glutReshapeFunc(reshape); // reshape 메서드 호출
   glutMainLoop();
   return 0;
